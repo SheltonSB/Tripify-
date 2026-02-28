ba9d12e0874e84c17baae9302c90a74fd4946b76
Rometh <romethsamarasinghe7@gmail.com>
2026-02-28 14:37:38 -0600

    Scaffolding step 1 edit docker compose

---
+++ backend/travel/src/test/java/com/tripify/travel/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripify.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
